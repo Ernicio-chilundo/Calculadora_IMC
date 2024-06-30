a826a0f8cb9bbc7e128b07e27178f116e3a76c6b
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-30 09:32:47 +0200

    loanding

---
+++ imc.js
@@ -0,0 +1,25 @@
+const calcular = document.getElementById("calcular")
+
+
+function imc (){
+    const nome = document.getElementById("nome").value
+    const altura = document.getElementById("altura").value
+    const peso = document.getElementById("peso").value
+    const resultado = document.getElementById("resultado")
+
+    if(nome !== "" && altura !== "" && peso !==""){
+        const valorIMC = (peso / (altura * altura)).toFixed(1)
+
+        let classificacao = ""
+        if(valorIMC < 18.5){
+            classificacao = "abaixo do peso."
+        }else if(valorIMC < 25){
+            classificacao = "com peso ideal. Parabens!!!"
+        }
+        resultado.textContent = `${nome} seu IMC e ${valorIMC} e voce esta ${classificacao}`
+    }else{
+        resultado.textContent="Preencha todos os campos!!!"
+    }
+}
+
+calcular.addEventListener("click", imc)
