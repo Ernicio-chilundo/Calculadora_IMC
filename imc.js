4e45903fd97eec9d7a3864b551ab4237247c72ff
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-06-30 09:41:25 +0200

    calculadora  imc  feita com sucesso

--- imc.js
+++ imc.js
@@ -15,6 +15,14 @@ function imc (){
             classificacao = "abaixo do peso."
         }else if(valorIMC < 25){
             classificacao = "com peso ideal. Parabens!!!"
+        }else if(valorIMC < 30){
+            classificacao = "levemente acima  do peso."
+        }else if(valorIMC < 35){
+            classificacao = "com obesidade grau 1."
+        }else if(valorIMC < 40){
+            classificacao ="com obesidade grau 2"
+        }else{
+            classificacao = "com obesidade grau 3, Cuidado!!!"
         }
         resultado.textContent = `${nome} seu IMC e ${valorIMC} e voce esta ${classificacao}`
     }else{
